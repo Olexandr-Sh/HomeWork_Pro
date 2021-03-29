5b5c458c24e7799c7d9a1a8678dc0a25f9bdc96a
Olexandr-Sh <shevtcov07@ukr.net>
2021-03-30 00:49:54 +0300

    Complete the first homework

---
+++ index.js
@@ -0,0 +1,85 @@
+console.log('Hello!');
+
+const myName = 'Olexandr';
+const secondName = 'Shevtsov';
+const birthdayDate = '16.05.1995';
+let height = 195;
+let weight = 92;
+let profession = 'lawyer, attorney assistant';
+let englishLevel = 'upper intermediate';
+let education = 'postgraduate of law';
+let scienceWork = 'Graduate work: Administrative and legal status of united territorial communities in Ukraine.';
+let hobby = 'guitar play, drawing portraits, studding english and IT';
+let visitedCountries = 'The Netherlands, Germany, Poland, Turkey, Greece, UAE';
+let target = 'work in Frontend sphere';
+let inspiration = 'my familly';
+let dream = 'own business';
+let personalQualities = 'fortitude';
+let car = null;
+let workInLondon = false;
+
+console.log(myName, typeof myName);
+console.log(secondName, typeof secondName);
+console.log(birthdayDate, typeof birthdayDate);
+console.log(height, typeof height);
+console.log(weight, typeof weight);
+console.log(profession, typeof profession);
+console.log(englishLevel, typeof englishLevel);
+console.log(education, typeof education);
+console.log(scienceWork, typeof scienceWork);
+console.log(hobby, typeof hobby);
+console.log(visitedCountries, typeof visitedCountries);
+console.log(target, typeof target);
+console.log(inspiration, typeof inspiration);
+console.log(dream, typeof dream);
+console.log(personalQualities, typeof personalQualities);
+console.log(car, typeof car);
+console.log(workInLondon, typeof workInLondon);
+
+let studentOlexandr = {
+  age: 25,
+  maritalStatus: 'married',
+  studentHillel: true,
+  basicCourse: 'passed',
+};
+
+console.log(studentOlexandr);
+
+alert('Hello!');
+
+let howAreYou = prompt('How are you?');
+console.log(howAreYou, typeof howAreYou);
+
+alert("Let's start the survey!)");
+
+let firstQuestion = confirm ('Do you like rock music?');
+console.log(firstQuestion, typeof firstQuestion);
+
+let secondQuestion = confirm('Do you often listen to music?');
+console.log(secondQuestion, typeof secondQuestion);
+
+let thirdQuestion = confirm('Do you like to sing songs with friends?');
+console.log(thirdQuestion, typeof thirdQuestion);
+
+let fourthQuestion = confirm('Do you like traveling?');
+console.log(fourthQuestion, typeof fourthQuestion);
+
+let fifthQuestion = confirm('Do you like beer?');
+console.log(fifthQuestion, typeof fifthQuestion);
+
+let theSixthQuestion = confirm("Do you know that in front of Rembrandt's house in Amsterdam there is a pub with one of the most delicious beers in Europe - Dark Amstel?");
+console.log(theSixthQuestion, typeof theSixthQuestion);
+
+let theSeventhQuestion = confirm('Do you like unique dishes?');
+console.log(theSeventhQuestion, typeof theSeventhQuestion);
+
+let theEighthQuestion = confirm('Have you ever been in the Myshi-blyahera restaurant? I recommend to visit it.');
+console.log(theEighthQuestion, typeof theEighthQuestion);
+
+let theNinthQuestion = confirm('Do you work in IT sphere during 10 years?');
+console.log(theNinthQuestion, typeof theNinthQuestion);
+
+let theTenthQuestion = confirm('Are you a professional?');
+console.log(theTenthQuestion, typeof theTenthQuestion);
+
+alert('Thank you for your attention)');
