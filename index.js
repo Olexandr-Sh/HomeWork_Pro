a4281271477a55bcb0ddb857f4ecf0ab7741472a
Olexandr-Sh <shevtcov07@ukr.net>
2021-04-02 00:37:23 +0300

    complete the second homework

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 console.log('Hello!');
 
-const myName = 'Olexandr';
-const secondName = 'Shevtsov';
-const birthdayDate = '16.05.1995';
-let height = 195;
-let weight = 92;
-let profession = 'lawyer, attorney assistant';
-let englishLevel = 'upper intermediate';
-let education = 'postgraduate of law';
-let scienceWork = 'Graduate work: Administrative and legal status of united territorial communities in Ukraine.';
-let hobby = 'guitar play, drawing portraits, studding english and IT';
-let visitedCountries = 'The Netherlands, Germany, Poland, Turkey, Greece, UAE';
-let target = 'work in Frontend sphere';
-let inspiration = 'my familly';
-let dream = 'own business';
-let personalQualities = 'fortitude';
-let car = null;
-let workInLondon = false;
+true + true + false // = 2 
+40 / '8' // = 5
+25 + 25 + ' років - це його вік' // = "50 років - це його вік"
+'радіус' + 20 // "радіус20"
+'vegetables' + + 'fruits' + 12 // = "vegetablesNaN12"
+' ' == 'true' // = false
+null == undefined // = true
+false === 1 // false
+'Alex' > 'Alexander' // false
+'Hi' > 'Hello' // true
+let a = "5";
+let b = 2;
+alert(b < a); // true
+'2225' > '221254' // true
+!!null == (5 && 0) > (27 || null) // = false == false > 27 /false == false / результат - true
+!(1 || 1) == !!(false && true) // = false == false / true
+// (null || 5) != (1 && 0) // = 5 != false / true
+// ( 1 || 2 ) == ( 3 && 0 ) // = true == false / false
+// [] + undefined + 25 // = "undefined25", враховуючи прочитане, розумію, що закоментовані приклади правильні, але консолі вони не подобаються
+
+
+
+
+
+
 
 
 
