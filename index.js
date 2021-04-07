7468c676bc7824fc29ee1b32af3e131e2fcd7b9e
Olexandr-Sh <shevtcov07@ukr.net>
2021-04-07 23:26:33 +0300

    added answer to first task from the third homework

--- index.js
+++ index.js
@@ -1,25 +1,49 @@
 console.log('Hello!');
 
-true + true + false // = 2 
-40 / '8' // = 5
-25 + 25 + ' років - це його вік' // = "50 років - це його вік"
-'радіус' + 20 // "радіус20"
-'vegetables' + + 'fruits' + 12 // = "vegetablesNaN12"
-' ' == 'true' // = false
-null == undefined // = true
-false === 1 // false
-'Alex' > 'Alexander' // false
-'Hi' > 'Hello' // true
-let a = "5";
-let b = 2;
-alert(b < a); // true
-'2225' > '221254' // true
-!!null == (5 && 0) > (27 || null) // = false == false > 27 /false == false / результат - true
-!(1 || 1) == !!(false && true) // = false == false / true
-// (null || 5) != (1 && 0) // = 5 != false / true
-// ( 1 || 2 ) == ( 3 && 0 ) // = true == false / false
-// [] + undefined + 25 // = "undefined25", враховуючи прочитане, розумію, що закоментовані приклади правильні, але консолі вони не подобаються
+// true + true + false // = 2 
+// 40 / '8' // = 5
+// 25 + 25 + ' років - це його вік' // = "50 років - це його вік"
+// 'радіус' + 20 // "радіус20"
+// 'vegetables' + + 'fruits' + 12 // = "vegetablesNaN12"
+// ' ' == 'true' // = false
+// null == undefined // = true
+// false === 1 // false
+// 'Alex' > 'Alexander' // false
+// 'Hi' > 'Hello' // true
+// let a = "5";
+// let b = 2;
+// alert(b < a); // true
+// '2225' > '221254' // true
+// !!null == (5 && 0) > (27 || null) // = false == false > 27 /false == false / результат - true
+// !(1 || 1) == !!(false && true) // = false == false / true
+// // (null || 5) != (1 && 0) // = 5 != false / true
+// // ( 1 || 2 ) == ( 3 && 0 ) // = true == false / false
+// // [] + undefined + 25 // = "undefined25", враховуючи прочитане, розумію, що закоментовані приклади правильні, але консолі вони не подобаються
 
+
+function name (a, b, c){
+    if ( a <=b ){
+        if (b <= c){
+            console.log (a, b, c);       
+        }
+        else if (a<=c){
+            console.log (a, c, b);
+        }
+        else {
+            console.log (c, a, b);
+        }
+    }
+    else if (c <= b){
+        console.log (c, b, a);       
+    }
+    else if (a <= c){
+        console.log (b, a, c);       
+    }
+    else {
+        console.log (b, c, a);       
+    }
+}
+name(68, 20, 67);
 
 
 
