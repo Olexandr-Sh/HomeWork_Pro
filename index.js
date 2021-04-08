30fcb61d2473e39b032c1ff6ee78573f322cb83d
Olexandr-Sh <shevtcov07@ukr.net>
2021-04-08 23:24:56 +0300

    finished second task. create temperature converter

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ console.log('Hello!');
 // // ( 1 || 2 ) == ( 3 && 0 ) // = true == false / false
 // // [] + undefined + 25 // = "undefined25", враховуючи прочитане, розумію, що закоментовані приклади правильні, але консолі вони не подобаються
 
+//first task of third lesson
 
 function name (a, b, c){
     if ( a <=b ){
@@ -44,6 +45,55 @@ function name (a, b, c){
     }
 }
 name(68, 20, 67);
+
+//second task of four lesson
+
+const celciusInput = document.querySelector('#celcius > input');
+const fahrenheitInput = document.querySelector('#fahrenheit > input');
+const kelvinInput = document.querySelector('#kelvin > input');
+
+function roundNum(num) {
+  return Math.round(num * 100) / 100;
+}
+
+// an example of the above
+// 3.1458769
+// when we multiply for 100 = 314.58769
+// when we divide we are converting to 3.14
+
+function celciusToFahrenheitAndKelvin() {
+  const cTemp = parseFloat(celciusInput.value);
+  const fTemp = (cTemp * (9 / 5)) + 32;
+  const kTemp = cTemp + 273.15;
+  fahrenheitInput.value = roundNum(fTemp);
+  kelvinInput.value = roundNum(kTemp);
+}
+
+function fahrenheitToCelciusAndKelvin() {
+  const fTemp = parseFloat(fahrenheitInput.value);
+  const cTemp = (fTemp - 32) * (5 / 9);
+  const kTemp = (fTemp + 459.67) * 5 / 9;
+  celciusInput.value = roundNum(cTemp);
+  kelvinInput.value = roundNum(kTemp);
+}
+
+function kelvinToCelciusAndFahrenheit() {
+  const kTemp = parseFloat(kelvinInput.value);
+  const cTemp = kTemp - 273.15;
+  const fTemp = 9/5 * (kTemp - 273) + 32;
+  celciusInput.value = roundNum(cTemp);
+  fahrenheitInput.value = roundNum(fTemp);
+}
+
+function main() {
+  celciusInput.addEventListener('input', celciusToFahrenheitAndKelvin);
+  fahrenheitInput.addEventListener('input', fahrenheitToCelciusAndKelvin);
+  kelvinInput.addEventListener('input', kelvinToCelciusAndFahrenheit);
+}
+
+main();
+
+
 
 
 
